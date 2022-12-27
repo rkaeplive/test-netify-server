b79ab4481fb846c17dcec751943922c28ab38b23
Роман <rkaeplive@gmail.com>
2022-12-27 15:35:55 +0300

    1

--- server.js
+++ server.js
@@ -6,17 +6,16 @@ app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ limit: "500mb" }));
 
 const connectionString =
-    "postgres://rkaeplive@ep-morning-bush-173522.eu-central-1.aws.neon.tech/velesstroy";
+    "postgres://rkaeplive:hLUs2AvH4qIX@ep-morning-bush-173522.eu-central-1.aws.neon.tech/velesstroy?ssl=true";
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-    connectionString,
-});
+const pool = new Pool({ connectionString });
 
 const allowCrossDomain = function (req, res, next) {
     res.header(
         "Access-Control-Allow-Origin",
         "https://silly-profiterole-99470e.netlify.app"
+        // "http://localhost:3000"
     );
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Credentials: true");
@@ -34,8 +33,8 @@ app.use(express.static(__dirname + "/UserModels"));
 //     // );
 //     res.status(200).json([1, 2, 3, 4, 5]);
 // });
-app.get("/", (req, res) => {
-    const data = pool.query(`SELECT * FROM ${config.DB_SCHEMA}.users`);
+app.get("/", async (req, res) => {
+    const data = await pool.query(`SELECT * FROM users`);
     res.status(200).json(data.rows);
 });
 
